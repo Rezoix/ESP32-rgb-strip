025209d18480875d93ce9041aafc35ab47571ac5
Saku Komulainen <sakuos@hotmail.fi>
2021-02-11 21:04:34 +0200

    Added modes, color cycling

--- Rgb_Strip_ESP32.ino
+++ Rgb_Strip_ESP32.ino
@@ -9,9 +9,18 @@
 const uint16_t PixelCount = 16;
 const uint8_t PixelPin = 26;  
 
+//Modes
+//Static: 0
+//Color cycle: 1
+uint8_t mode = 1; 
+
 NeoPixelBus<NeoBrgFeature, Neo400KbpsMethod> strip(PixelCount, PixelPin);
 
-int brightness = 255; //0-255
+uint8_t brightness = 255; //0-255
+uint8_t hue = 0;
+double lightness = 0.7;
+double value = 1;
+double saturation = 1;
 
 // Server stuff
 const char* ssid = "***REMOVED***";
@@ -53,7 +62,7 @@ void setup()
   server.on("/get", HTTP_POST, [](AsyncWebServerRequest *request){
     String inputMessage;
 
-    if (request->hasParam("res")) {
+    if (request->hasParam("res") && mode == 0) {
       inputMessage = request->getParam("res")->value();
       request->send(200);
       RgbColor color = parseHex(inputMessage);
@@ -62,6 +71,12 @@ void setup()
           strip.SetPixelColor(pixel, color);
       }
       strip.Show();
+    } else if (request->hasParam("mode")) {
+      if (request->hasParam("mode")) {
+        inputMessage = request->getParam("mode")->value();
+        request->send(200);
+        mode = inputMessage.toInt();
+      }
     } else {
       request->send(400);
     }
@@ -83,9 +98,119 @@ RgbColor parseHex(String hex) {
   return RgbColor(0,0,0);
 }
 
-void loop()
-{
+RgbColor HslToRGB(uint8_t hue, double s, double l) {
+  uint8_t r, g, b = 0;
+  
+  double v1, v2;
+  double h = hue/360.0;
 
+  v2 = (s + l) - (l * s);
+  v1 = 2 * l - v2;
+
+  r = (uint8_t)(255 * HueToRgb(v1, v2, h + (1.0 / 3)));
+  g = (uint8_t)(255 * HueToRgb(v1, v2, h));
+  b = (uint8_t)(255 * HueToRgb(v1, v2, h - (1.0 / 3)));
+
+  Serial.print(r);
+  Serial.print(", ");
+  Serial.print(g);
+  Serial.print(", ");
+  Serial.println(b);
+
+  return RgbColor(r, g, b);
 }
 
+double HueToRgb(double v1, double v2, double h) {
+  if (h < 0) {
+    h += 1;
+  }
+  if (h > 1) {
+    h -= 1;
+  }
+  if ((6 * h) < 1) {
+    return (v1 + (v2 - v1) * 6 * h);
+  }
+  if ((2 * h) < 1) {
+    return (v2);
+  }
+  if ((3 * h) < 2) {
+    return (v1 + (v2 - v1) * ((2.0 / 3) - h) * 6);
+  }
 
+  return v1;
+}
+
+RgbColor HsvToRgb(uint8_t hue, double sat, double val) {
+  double r, g, b = 0;
+  double h = hue % 360;
+  double hh = h;
+  hh /= 60.0;
+
+  long i = (long)hh;
+  double ff = hh - i;
+  double p = val * (1.0 - sat);
+  double q = val * (1.0 - (sat * ff));
+  double t = val * (1.0 - (sat * (1.0 - ff)));
+
+  switch (i) {
+    case 0:
+      r = val;
+      g = t;
+      b = p;
+      break;
+    case 1:
+      r = q;
+      g = val;
+      b = p;
+      break;
+    case 2:
+      r = p;
+      g = val;
+      b = t;
+      break;
+    case 3:
+      r = p;
+      g = q;
+      b = val;
+      break;
+    case 4:
+      r = t;
+      g = p;
+      b = val;
+      break;
+    case 5:
+    default:
+      r = val;
+      g = p;
+      b = q;
+      break;
+  }
+
+  r = r * 255;
+  g = g * 255;
+  b = q * 255;
+
+  Serial.print(r);
+  Serial.print(", ");
+  Serial.print(g);
+  Serial.print(", ");
+  Serial.println(b);
+
+  return RgbColor(r, g, b);
+}
+
+void loop()
+{
+  if (mode == 1) {
+    hue = hue + 1;
+
+    RgbColor color = HsvToRgb(hue, saturation, value);
+
+    for (uint16_t pixel = 0; pixel < PixelCount; pixel++)
+      {
+          strip.SetPixelColor(pixel, color);
+      }
+    strip.Show();
+    delay(10);
+  }
+}
